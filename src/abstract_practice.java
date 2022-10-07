2bd2edaa8391e10dde56b49c5b5aa074d92fc5bb
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-10-07 22:22:40 +0530

    commit

--- src/abstract_practice.java
+++ src/abstract_practice.java
@@ -1,5 +1,22 @@
+interface Areas{
+    void area(float a, float b);
+}
 
+class Triangle implements Areas{
+    public void area(float a, float b){
+        System.out.println("RAea is " + 0.5*a*b);
+    }
+}
+class Rect implements Areas{
+    public void area(float a, float b){
+        System.out.println(a*b);
+    }
+}
 
 
 public class abstract_practice {
+    public static void main(String[] args) {
+        Areas triangle = new Triangle();
+        triangle.area(5,4);
+    }
 }
