e3a0d6d78fe4bfd293f154256638f59e9b58d164
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-10-07 22:01:42 +0530

    commit

--- src/interface_examples.java
+++ src/interface_examples.java
@@ -1,7 +1,14 @@
 interface ex{
+    int a =20;
+}
+class bf implements ex{
 
 }
 
 
 public class interface_examples {
+    public static void main(String[] args) {
+        bf ob = new bf();
+        System.out.println(ob.a);
+    }
 }
