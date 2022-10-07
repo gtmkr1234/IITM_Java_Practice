0bd355c034830194f575432164a48364bbddadbc
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-10-07 22:06:50 +0530

    commit

--- src/interface_examples.java
+++ src/interface_examples.java
@@ -12,6 +12,7 @@ class bf implements ex,best{
 public class interface_examples {
     public static void main(String[] args) {
         bf ob = new bf();
+        //ob.a = 54; this will generate an error because the local variables are final inside the interface..
         System.out.println(ob.a);
         System.out.println(ob.v);
     }
