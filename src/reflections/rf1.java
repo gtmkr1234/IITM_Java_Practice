6ac22e806bfc8620481e6b0be8cd2bc82533f29d
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2022-11-16 10:05:02 +0530

    added more examples of reflection

--- src/reflections/rf1.java
+++ src/reflections/rf1.java
@@ -8,3 +8,15 @@ public class rf1 {
         System.out.println(a);
     }
 }
+
+class r2{
+    public static boolean isEqualClass(Object o1, Object o2){
+        Class c1 = o1.getClass();
+        Class c2 = o2.getClass();
+        return (c1==c2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isEqualClass(new Integer(78), new Integer(45)));
+    }
+}
