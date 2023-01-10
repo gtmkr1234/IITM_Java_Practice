04547d55a8f86e63ca8699218ecb9df879015bbd
Krishna Gautam <73680786+gtmkr1234@users.noreply.github.com>
2023-01-10 15:08:45 +0530

    added more examples of reflection

---
+++ src/HashMaps/Main3.java
@@ -0,0 +1,13 @@
+package HashMaps;
+import java.util.*;
+
+public class Main3 {
+    public static void main(String[] args) {
+        HashMap<Integer, String> hm = new HashMap<>();
+        hm.put(1,"Red");
+        hm.put(2,"Green");
+        hm.put(3,"Blue");
+        hm.put(4,"White");
+        System.out.println("Size of the hash map : "+hm.size());
+    }
+}
